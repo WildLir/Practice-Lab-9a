3d7f0e17d6cf484c4f4dd0806ea51f6bde453b9d
WildLir <banan4ik210@gmail.com>
2020-07-01 15:00:09 +0300

    First commit

---
+++ Lab 9a (practice)/Lab 9a (practice).cpp
@@ -0,0 +1,23 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#include<iostream>
+using namespace std;
+
+void input(int**& p, int n)
+{
+	int i, j;
+	p = new int* [n];
+	for (i = 0; i < n; i++)
+		p[i] = new int[n]();
+	cout << "Введите элементы массива" << endl;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			cin >> p[i][j];
+	cout << "Исходная матрица" << endl;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			cout << p[i][j] << " ";
+		cout << endl;
+	}
+}
