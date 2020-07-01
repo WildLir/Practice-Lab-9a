8af3afae46c0370627a2a3967db822e3023cd7d7
WildLir <banan4ik210@gmail.com>
2020-07-01 15:46:21 +0300

    Third commit

--- Lab 9a (practice)/Lab 9a (practice).cpp
+++ Lab 9a (practice)/Lab 9a (practice).cpp
@@ -34,3 +34,85 @@ void output(int** p, int n)
 	}
 	delete[]p;
 }
+
+void Change(int** p, int n)
+{
+	int i, j,k,g;
+	int max=p[0][0];
+	int buf;
+	for (i = 0; i < n; i++)
+		for (j = 0 ; j < n; j++)
+		{
+			if (max < p[i][j])
+			{
+				k = i;
+				g = j;
+			}
+		}
+
+	if (g > n / 2)
+		while (g != n / 2 )
+		{
+			for (i = 0; i < n - 1; i++)
+			{
+				buf = p[k][i];
+				p[k][i] = p[k][i + 1];
+				p[k][i + 1] = buf;
+			}
+			g--;
+		}
+	else
+		while (g != n / 2+2 )
+		{
+			for (i = 0; i < n - 1; i++)
+			{
+				buf = p[k][i];
+				p[k][i] = p[k][i - 1];
+				p[k][i - 1] = buf;
+			}
+			g++;
+		}
+
+	if (k > n / 2 )
+	{
+		while (k != n / 2 )
+		{
+			for (i = 0; i < n - 1; i++)
+			{
+				buf = p[i][g];
+				p[i][g] = p[i + 1][g];
+				p[i + 1][g] = buf;
+			}
+			k--;
+		}
+	}
+	else while (k != n / 2 )
+	{
+		for (i = n; i > 1; i++)
+		{
+			buf = p[i][g];
+			p[i][g] = p[i - 1][g];
+			p[i - 1][g] = buf;
+		}
+		k++;
+	}
+
+	
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "");
+	int** p = 0;
+	int n;
+	cout << "Введите разрядность матрицы(Должна быть нечетной)" << endl;
+	cin >> n;
+	while (n % 2 == 0)
+	{
+		cout << "Разрядность четная, введите другое число" << endl;
+		cin >> n;
+	}
+	input(p, n);
+	Change(p, n);
+	output(p, n);
+}
